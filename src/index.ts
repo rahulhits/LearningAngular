8d1ad14fb59b8c035ccbc7f4873d3358f5a88f5f
rahulhits <rahulhits2013@gmail.com>
2024-09-08 00:32:23 +0530

    On7hSepth

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+function Demo()
+{
+    var x;
+    x=10;
+    if(x==10)
+    {
+        var y=20;
+    }
+    console.log("x"+x+"\n"+"Y"+y);
+}
+// Demo();
+
+function varHoisting()
+{
+    a=10;
+    console.log("x="+a)
+    var a;
+}
+varHoisting();
