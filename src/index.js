d3212761c518163c1eb99a0b5dde14d10a992683
rahulhits <rahulhits2013@gmail.com>
2024-09-08 04:16:35 +0530

    Learning DataType and Array

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
-function Demo() {
-    var x;
-    x = 10;
-    if (x == 10) {
-        var y = 20;
-    }
-    console.log("x" + x + "\n" + "Y" + y);
-}
-// Demo();
-function varHoisting() {
-    a = 10;
-    console.log("x=" + a);
-    var a;
-}
-varHoisting();
+var collecction = [10, "A"];
+console.log(collecction);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
-function Demo()
-{
-    var x;
-    x=10;
-    if(x==10)
-    {
-        var y=20;
-    }
-    console.log("x"+x+"\n"+"Y"+y);
-}
-// Demo();
+// var collecction:any[]=[10,"A"];
+// console.log(collecction);
 
-function varHoisting()
-{
-    a=10;
-    console.log("x="+a)
-    var a;
-}
-varHoisting();
+var collecction:any[]=new Array(10,);
+console.log(collecction);
+
+
+
