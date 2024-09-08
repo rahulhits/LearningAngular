9ccb6202df8f759c0c2dae0b55265bc2ba90a4a7
rahulhits <rahulhits2013@gmail.com>
2024-09-09 00:48:08 +0530

    property Method and Constructor

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-var collecction = [10, "A"];
-console.log(collecction);
+var Product = /** @class */ (function () {
+    function Product() {
+        this.Name = "TV";
+        this.Price = 45.000;
+        this.Qty = 2;
+    }
+    Product.prototype.Total = function () {
+        return this.Qty * this.Price;
+    };
+    Product.prototype.Print = function () {
+        console.log("Name".concat(this.Name, "\nPrice=").concat(this.Price, "\nQty=").concat(this.Qty));
+    };
+    return Product;
+}());
+var obj = new Product();
+obj.Print();
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-// var collecction:any[]=[10,"A"];
-// console.log(collecction);
-
-var collecction:any[]=new Array(10,);
-console.log(collecction);
-
-
-
+class Product
+{
+    public Name:string="TV";
+    public Price:number=45.000;
+    public Qty:number=2
+    public Total():number{
+        return this.Qty * this.Price;
+    }
+    public Print():void{
+        console.log(`Name${this.Name}\nPrice=${this.Price}\nQty=${this.Qty}`);
+            }
+}
+let obj=new Product();
+obj.Print();
